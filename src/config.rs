b6ce1098a42bb4180a4db34c670c1607d9302820
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2022-08-24 23:42:48 +0100

    add domain to config

--- src/config.rs
+++ src/config.rs
@@ -3,11 +3,14 @@ use std::env;
 const DEFAULT_KEY: &str = "changeme";
 const DEFAULT_PATH: &str = "sharex.db";
 const DEFAULT_SS_PATH: &str = "screenshots/";
+const DEFAULT_DOMAIN: &str = "ss.ussr.pl";
 
+// TODO: Learn macros to simplify this whole thing.
 pub struct Config {
     pub private_key: String,
     pub db_path: String,
     pub screenshot_path: String,
+    pub domain: String,
 }
 
 impl Config {
@@ -15,11 +18,13 @@ impl Config {
         let private_key = env::var("SX_PRIVATE_KEY").unwrap_or(DEFAULT_KEY.to_string());
         let db_path = env::var("SX_DB_PATH").unwrap_or(DEFAULT_PATH.to_string());
         let screenshot_path = env::var("SX_SS_PATH").unwrap_or(DEFAULT_SS_PATH.to_string());
+        let domain = env::var("SX_DOMAIN").unwrap_or(DEFAULT_DOMAIN.to_string());
 
         Self {
             private_key: private_key,
             db_path: db_path,
             screenshot_path: screenshot_path,
+            domain: domain,
         }
     }
 }
